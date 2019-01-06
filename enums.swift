d575b62de018a4898503ade9312e94758d9325ca
onweru <onewesh@gmail.com>
2019-01-06 19:37:23 +0300

    update enum

--- enums.swift
+++ enums.swift
@@ -2,19 +2,24 @@ enum Rank: Int {
   case ace = 1
   case one, two, three, four, five
   case king, queen, bishop
+
+  func simpleDescription() -> String {
+    switch self {
+      case .ace: return "Ace"
+      case .king: return "King"
+      case .queen: return "Queen"
+      case .bishop: return "Bishop"
+      default: return String(self.rawValue)
+    }
+  }
 }
 
-// func simpleDescription() -> String {
-//   switch self {
-//     case .ace: return "Ace"
-//     case .king: return "King"
-//     case .queen: return "Queen"
-//     case .bishop: return "Bishop"
-//     default: return String(self.rawValue)
-//   }
-// }
 
 let ace = Rank.five
 let result = ace.rawValue
 
 print(result)
+
+if let convertedRank = Rank(rawValue: 3) {
+  let threeDescription = convertedRank.simpleDescription
+}
