1e53ae1369ca28595ff900a820a0a4b1a0bc9a53
onweru <onewesh@gmail.com>
2019-01-06 19:32:12 +0300

    add file

---
+++ enums.swift
@@ -0,0 +1,20 @@
+enum Rank: Int {
+  case ace = 1
+  case one, two, three, four, five
+  case king, queen, bishop
+}
+
+// func simpleDescription() -> String {
+//   switch self {
+//     case .ace: return "Ace"
+//     case .king: return "King"
+//     case .queen: return "Queen"
+//     case .bishop: return "Bishop"
+//     default: return String(self.rawValue)
+//   }
+// }
+
+let ace = Rank.five
+let result = ace.rawValue
+
+print(result)
