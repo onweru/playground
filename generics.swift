c01d63b7f435d8528e6d06ea3652dea0df439868
onweru <onewesh@gmail.com>
2019-01-07 14:08:49 +0300

    add equatable generic

--- generics.swift
+++ generics.swift
@@ -9,3 +9,20 @@ func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
 
 let knocks = makeArray(repeating: "knock", numberOfTimes: 4)
 print(knocks)
+
+
+// more complex generic example
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool where T.Element: Equatable, T.Element == U.Element {
+  for lhsItem in lhs {
+    for rhsItem in rhs {
+      if lhsItem == rhsItem {
+        return true
+      }
+    }
+  }
+  return false
+}
+
+let example = anyCommonElements([1, 2, 3], [3])
+print(example)
