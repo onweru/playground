ff7bd23dd6490139be68ade11c92a5b871ae274c
onweru <onewesh@gmail.com>
2019-01-21 20:43:47 +0300

    add closures

--- closures.swift
+++ closures.swift
@@ -8,3 +8,16 @@ func backward(_ s1: String, _ s2: String) -> Bool {
 var  revervedNames = names.sorted(by: backward)
 
 print(revervedNames)
+
+// example 2 ~ (somewhat more concise)
+
+revervedNames = names.sorted(by: {(s1: String, s2: String) -> Bool in return s1 > s2})
+
+print(revervedNames)
+
+// example 3 ~ (variation of #3)
+
+let locations = ["Nairobi", "Arusha", "Tunis", "Moscow", "Tel Aviv"]
+ 
+var arrangedLocations = locations.sorted(by: {(locale1, locale2) -> Bool in return locale1 < locale2})
+print(arrangedLocations)
