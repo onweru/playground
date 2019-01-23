e28f8cc25ee9b7f0ed3f8f886daa0f88960b6a8c
onweru <onewesh@gmail.com>
2019-01-23 21:48:48 +0300

    update file

--- enums.swift
+++ enums.swift
@@ -203,3 +203,13 @@ let product = ArithmeticExpression.multiplication(sum, ArithmeticExpression.numb
 
 print(sum)
 print(product)
+
+func evaluate(_ expression: ArithmeticExpression) -> Int {
+  switch expression {
+    case let .number(value): return value
+    case let .addition(left, right): return evaluate(left) + evaluate(right)
+    case let .multiplication(left, right): return evaluate(left) * evaluate(right)
+  }
+}
+
+print(evaluate(product))
