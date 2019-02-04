1b2a2e20280adb2ac8ce301b60711df659cbf93a
onweru <onewesh@gmail.com>
2019-02-04 19:32:19 +0300

    propagate failable initializer

--- init.swift
+++ init.swift
@@ -63,3 +63,37 @@ let unknownValue = TemperatureValue(rawValue: "X")
 if unknownValue == nil {
   print("This is not a defined temperature value, so initialization failed")
 }
+
+// Propagate initialization failure
+class Product {
+  let name: String
+  init?(name: String) {
+    if name.isEmpty { return nil }
+    self.name = name
+  }
+}
+
+class CartItem: Product {
+  let quantity: Int 
+  init?(name: String, quantity: Int) {
+    if quantity < 1 { return nil }
+    self.quantity = quantity
+    super.init(name: name)
+  }
+}
+
+if let twoSocks = CartItem(name: "sock", quantity: 2) {
+  print("Item: \(twoSocks.name), quantity: \(twoSocks.quantity)")
+}
+
+if let zeroShirts = CartItem(name: "shirt", quantity: 0) {
+  print("Item: \(zeroShirts.name), quantity: \(zeroShirts.quantity)")
+} else {
+  print("Unable to initialize zero shirts")
+}
+
+if let oneUnnamed = CartItem(name: "", quantity: 1) {
+  print("Item: \(oneUnnamed.name), quantity: \(oneUnnamed.quantity)")
+} else {
+  print("Unable to initialize one unnamed product")
+}
