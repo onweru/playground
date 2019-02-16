3780dc7ffe1098ad70d84769c326f9d96eb0f9b6
weru <onewesh@gmail.com>
2019-02-16 19:38:35 +0300

    update

--- struct.swift
+++ struct.swift
@@ -96,33 +96,3 @@ struct Queue<Element> {
     return array.remove(at: 0)
   }
 }
-
-
-<?php 
-/*
-* Displays all the posts entries
-*
-*/
-?>
-
-<?php get_header() ?>
-	
-	<?php  get_template_part('includes/masthead') ?>
-	
-	<!-- posts list -->
-	<div id="posts-list" class="clearfix">        	
-		<?php get_template_part('includes/loop' ) ?>
-		
-		<!-- page-navigation -->
-		<div class="page-navigation clearfix">
-			<div class="nav-next"><?php  next_posts_link('&#8592; Older Entries') ?></div>
-			<div class="nav-previous"><?php previous_posts_link('Newer Entries &#8594;') ?></div>
-		</div>
-		<!--ENDS page-navigation -->
-	
-	</div>
-	<!-- ENDS posts list -->
-	
-	<?php get_sidebar() ?>
-	
-<?php get_footer() ?>
