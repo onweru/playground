1fb264940df5f4534007811ee1808b61f2e91e53
weru <onewesh@gmail.com>
2019-02-18 15:55:02 +0300

    update file

--- protocol.swift
+++ protocol.swift
@@ -24,3 +24,24 @@ class StarShip: FullyNamed {
 
 var ncc1701 = StarShip(name: "Enterprise", prefix: "USS")
 print(ncc1701.fullName)
+
+// protocols & methods
+
+protocol RandomNumberGenerator {
+  func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+  var lastRandom = 42.0
+  let m = 139968.0
+  let a = 3877.0
+  let c = 39573.0
+  func random() -> Double {
+    lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy: m))
+    return lastRandom / m
+  }
+}
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number: \(generator.random())")
+print("And another one: \(generator.random())")
