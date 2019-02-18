0ba8bdd57e74a546fd57acef81c080236db60fae
weru <onewesh@gmail.com>
2019-02-18 16:33:08 +0300

    update file

--- protocol.swift
+++ protocol.swift
@@ -100,3 +100,49 @@ var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
 for _ in 1...5 {
   print("Random dice rol is \(d6.roll())")
 }
+
+// Delegation
+
+protocol DiceGame {
+  var dice: Dice { get }
+  func play()
+}
+
+protocol DiceGameDelegate: AnyObject {
+  func gameDidStart(_ game: DiceGame)
+  func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+  func gameDidEnd(_ game: DiceGame)
+}
+
+class SnakesAndLadders: DiceGame {
+  let finalSquare = 25
+  let dice = Dice(sides: 6, generator: LinearCongruentialGenerator())
+  var square = 0
+  var board: [Int]
+
+  init() {
+    board = Array(repeating: 0, count: finalSquare + 1)
+    board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+    board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+  }
+
+  weak var delegate: DiceGameDelegate?
+  func play() {
+    square = 0
+    delegate?.gameDidStart(self)
+    gameLoop: while square != finalSquare {
+      let diceRoll = dice.roll()
+      delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+      switch square + diceRoll {
+        case finalSquare:
+          break gameLoop
+        case let newSquare where newSquare > finalSquare:
+          continue gameLoop
+        default:
+          square += diceRoll
+          square += board[square]
+      }
+    }
+    delegate?.gameDidEnd(self)
+  }
+}
