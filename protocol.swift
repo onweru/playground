bb27a6ee7a8436ace10ab8e45375509a9709cc9c
weru <onewesh@gmail.com>
2019-02-18 16:56:36 +0300

    update file

--- protocol.swift
+++ protocol.swift
@@ -146,3 +146,28 @@ class SnakesAndLadders: DiceGame {
     delegate?.gameDidEnd(self)
   }
 }
+
+class DiceGameTracker: DiceGameDelegate {
+  var numberOfTurns = 0
+  func gameDidStart(_ game: DiceGame) {
+    numberOfTurns = 0
+    if game is SnakesAndLadders {
+      print("Started new game of Snakes and Ladders")
+    }
+    print("The game is using a \(game.dice.sides)-sided dice")
+  }
+
+  func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+    numberOfTurns += 1
+    print("Rolled a \(diceRoll)")
+  }
+
+  func gameDidEnd(_ game: DiceGame) {
+    print("The game lasted for \(numberOfTurns) turns")
+  }
+}
+
+let tracker = DiceGameTracker()
+let game = SnakesAndLadders()
+game.delegate = tracker
+game.play()
