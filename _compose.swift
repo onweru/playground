35e2bc933f92bf1e288062a6a72e25f0f81434be
weru <onewesh@gmail.com>
2019-02-19 14:04:19 +0300

    update file

--- _compose.swift
+++ _compose.swift
@@ -47,3 +47,81 @@ let seattle = City(name: "Seattle", latitude: 47.6, longitude: -122.3)
 beginConcert(in: seattle)
 
 // check for protocol conformance
+
+protocol HasArea {
+  var area: Double { get }
+}
+
+class Circle: HasArea {
+  let pi = 3.1415927
+  var radius: Double
+  var area: Double { return pi * radius * radius }
+  
+  init(radius: Double ) {
+    self.radius = radius
+  }
+}
+
+class Country: HasArea {
+  var area: Double
+
+  init(area: Double) {
+    self.area = area
+  }
+}
+
+class Animal {
+  var legs: Int
+  
+  init(legs: Int) {
+    self.legs = legs
+  }
+}
+
+let objects: [AnyObject] = [
+  Circle(radius: 2.0),
+  Country(area: 243_610),
+  Animal(legs: 4)
+]
+
+for object in objects {
+  if let objectWithArea = object as? HasArea {
+    print("Area is \(objectWithArea.area)")
+  } else {
+    print("Something that doesn't have an area")
+  }
+}
+
+// optional protocol requirements
+
+import Foundation
+
+@objc protocol CounterDataSource {
+  @objc optional func increment(forCount count: Int) -> Int
+  @objc optional var fixedIncrement: Int { get }
+}
+
+class Counter {
+  var count = 0
+  var dataSource: CounterDataSource?
+
+  func increment() {
+    if let amount = dataSource?.increment?(forCount: count) {
+      count += amount
+    } else if let amount = dataSource?.fixedIncrement {
+      count += amount
+    }
+  }
+}
+
+class ThreeSource: NSObject, CounterDataSource {
+  let fixedIncrement = 3
+}
+
+var counter = Counter()
+counter.dataSource = ThreeSource()
+
+for _ in 1...4 {
+  counter.increment()
+  print(counter.count)
+}
