bc9c6ac7453d94a9409ddd2e3e0457d779d2c00f
weru <onewesh@gmail.com>
2019-02-23 21:51:27 +0300

    update

--- generics_.swift
+++ generics_.swift
@@ -13,11 +13,22 @@ print("SomeInt is now \(someInt), and anotherInt is now \(anotherInt)")
 func swapTwoStrings(_ a: inout String, _ b: inout String) {
   let temporaryA = a
   a = b
-  b = temporaryB
+  b = temporaryA
 }
 
 func swapTwoDoubles(_ a: inout Double, _ b: inout Double) {
   let temporaryA = a
   a = b
-  b = temporaryB
+  b = temporaryA
 }
+
+func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
+  let temporaryA = a
+  a = b
+  b = temporaryA
+}
+
+var someString = "Hola"
+var anotherString = "Amigo"
+swapTwoValues(&someString, &anotherString)
+print("SomeString is now \(someString), and anotherString is now \(anotherString)")
