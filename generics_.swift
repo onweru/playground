e4babd45599dd45f5634b348f313a8de8c0f97c3
weru <onewesh@gmail.com>
2019-02-23 22:26:40 +0300

    update file

--- generics_.swift
+++ generics_.swift
@@ -67,3 +67,19 @@ extension Stack {
 if let topItem = stackOfStrings.topItem {
   print("The top item on the stacj is \(topItem).")
 }
+
+// generic type constraints
+
+func findIndex<T: Equatable>(of valueToFind: T, in array:[T]) -> Int? {
+  for (index, value) in array.enumerated() {
+    if value == valueToFind {
+      return index
+    }
+  }
+  return nil
+}
+
+let doubleIndex = findIndex(of: 9.3, in: [3.14159, 0.1, 0.25])
+print(doubleIndex)
+
+let stringIndex = findIndex(of: "Andrea", in: ["Mike", "Malcolm", "Andrea"])
